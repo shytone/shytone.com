83acb9fbbea0633bbaa86e888872f03354dcfcaa
shytone <shytone@users.noreply.github.com>
2025-11-17 13:30:28 +0000

    deploy: c1c47058fdf62a1f592871789908e625c537f57e

--- assets/search.js
+++ assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Hello World",'url':"/2024/01/01/hello-world.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome",'url':"/2018/07/01/welcome.html"},{'title':"Hello World",'url':"/2024/01/01/hello-world.html"}]};
